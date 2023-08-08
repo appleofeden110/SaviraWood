fe83fb72d15dc3b1cb8a4907f76f0af2004c1ff6
nazarkaniuka6 <nazarkaniuka6@gmail.com>
2023-08-08 18:37:06 +0100

    changed name of the frontend folder + added cors ignore + resolvving the issue with the :to

---
+++ front-nuxt/SaviraWood/pages/product/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in data">
+                <nuxt-link to="/product/`${p.id}`">{{ p.name }}</nuxt-link>
+            </div>
+        </div>
+    </div>
+
+    
+</template>
+
+<script setup>
+const { data } = await useFetch('http://127.0.0.1:5000/product'); 
+
+
+</script>
+
+<style scoped>
+
+</style>
